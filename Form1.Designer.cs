be90fe427bf2cf6f255a7b65cd47ec94ca0679fd
UszOszekk <92518862+UszOszekk@users.noreply.github.com>
2022-07-29 21:43:21 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace set_display_resolutions
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Resolution = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.height = new System.Windows.Forms.NumericUpDown();
+            this.width = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.height)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.width)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Selected Resolution:";
+            // 
+            // Resolution
+            // 
+            this.Resolution.AutoSize = true;
+            this.Resolution.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Resolution.Location = new System.Drawing.Point(171, 123);
+            this.Resolution.Name = "Resolution";
+            this.Resolution.Size = new System.Drawing.Size(176, 20);
+            this.Resolution.TabIndex = 5;
+            this.Resolution.Text = "Please select resolution";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(135, 76);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 31);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Display window";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // height
+            // 
+            this.height.Location = new System.Drawing.Point(174, 50);
+            this.height.Maximum = new decimal(new int[] {
+            1080,
+            0,
+            0,
+            0});
+            this.height.Minimum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.height.Name = "height";
+            this.height.Size = new System.Drawing.Size(120, 20);
+            this.height.TabIndex = 8;
+            this.height.Value = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.height.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // width
+            // 
+            this.width.Location = new System.Drawing.Point(48, 50);
+            this.width.Maximum = new decimal(new int[] {
+            1920,
+            0,
+            0,
+            0});
+            this.width.Minimum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.width.Name = "width";
+            this.width.Size = new System.Drawing.Size(120, 20);
+            this.width.TabIndex = 9;
+            this.width.Value = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.width.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(174, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "height";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "width";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(359, 161);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.width);
+            this.Controls.Add(this.height);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Resolution);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.height)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.width)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Resolution;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown height;
+        private System.Windows.Forms.NumericUpDown width;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace set_display_resolutions
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Form2 display = new Form2();
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            this.h = height.Value;
+            this.w = width.Value;
+            int h2 = (int)h;
+            int w2 = (int)w;
+            display.H = h2;
+            display.W = w2;
+            display.Show();
+            this.Hide();
+            var box = MessageBox.Show("The first window is hidden now click OK to close program","Information",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            if(box == DialogResult.OK)
+            {
+                this.Close();
+            }
+        }
+        NumericUpDown sans = new NumericUpDown();
+        public void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            decimal w = width.Value;
+
+            this.h = height.Value;
+            Resolution.Text = w + " x " + h;
+
+        }
+        public decimal w{ get; set; }
+        public decimal h { get; set; }
+        public void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            
+            decimal h = height.Value;
+            this.w = width.Value;
+            Resolution.Text = w + " x " + h;
+
+            
+         
+        }
+
+
+    }
+}
